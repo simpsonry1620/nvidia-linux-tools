e0146fa53d7650de778e526d8852d5bcfeb2f2e8
simpsonry1620 <60401213+simpsonry1620@users.noreply.github.com>
2021-01-26 14:54:16 -0500

    Update blacklist.sh
    Added the update for initramfs

--- blacklist.sh
+++ blacklist.sh
@@ -4,3 +4,8 @@
 # blacklist the nouveau drivers to prevent issues with cuda and other NVIDIA tools.
 sudo bash -c "echo blacklist nouveau > /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
 sudo bash -c "echo options nouveau modeset=0 >> /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
+
+# regenerate initramfs
+sudo update-initramfs -u
+
+echo The system should be rebooted for updates to take effect.
